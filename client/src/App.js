ae00d9ad04158d96c024e59dcc9280f7795f47d3
Dominik Stork <dominik.stork@gmx.de>
2022-05-02 23:10:26 +0200

    Board centered

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import Game from './Game.js';
+import './css/App.css';
 
 function App({backend}) {
   return (
-	<Game
-			iAmPlayer1="true"
-	  	backend={backend}
-		/>
+		<div className="App">
+			<Game
+				iAmPlayer1="true"
+				backend={backend}
+			/>
+		</div>
   );
 }
 
--- todo.md
+++ todo.md
@@ -2,4 +2,5 @@
 
 * [DONE] When loading a tab into a finished game, the winning row/column should be highlighted automatically (and the game state set accordingly)
 * [DONE] Include a "rematch" button
+* [DONE] center the board
 * tests
